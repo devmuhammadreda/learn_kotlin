0c1ec9eb082359718c13bf0bd73447ab6228230f
mrida <mrida@tamweely.com.eg>
2025-11-30 14:05:58 +0200

    run catching

--- src/exceptions_handling2.kt
+++ src/exceptions_handling2.kt
@@ -1,4 +1,14 @@
 fun main() {
+  val result : Result<Boolean> = runCatching { login("admin" , "123456") }
+    result.onSuccess {
 
+        println("Success $it")
+    }
+    result.onFailure {
+        println("Error ${it.message}")
+    }
 }
 
+fun login (userName : String , password : String): Boolean{
+    return userName == "admin" && password == "123456"
+}
